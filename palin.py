c67b4dfaf9e35dedf7edd41cb67ac4c4c910f52f
Pabitha <39622249+Pabitha@users.noreply.github.com>
2018-05-27 22:54:04 +0530

    Update palin.py

--- palin.py
+++ palin.py
@@ -1,11 +1,11 @@
-n=int(input())
-temp=n
-rev=0
-while(n>0):
-    dig=n%10
-    rev=rev*10+dig
-    n=n//10
-if(temp==rev):
+a=int(raw_input())
+temp=a
+c=0
+while(a>0):
+    b=a%10
+    c=c*10+b
+    a=a/10
+if(temp==c):
     print("yes")
 else:
     print("no")
