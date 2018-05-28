825257241e6fb53205d0870709be5412d57fe43f
Pabitha <39622249+Pabitha@users.noreply.github.com>
2018-05-28 20:00:16 +0530

    Create prime2.py

---
+++ prime2.py
@@ -0,0 +1,10 @@
+a=int(raw_input())
+b=int(raw_input())
+for i in range(a,b):
+    if(a>1):
+        for i in range(2,b+1):
+        	if(a%i)==0:
+        	    break
+        	else:
+        		print(a)
+        		
