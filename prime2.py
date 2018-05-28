34ca0f14a4ccc5974e2f4f0a89ef46f9e79df877
Pabitha <39622249+Pabitha@users.noreply.github.com>
2018-05-28 23:44:18 +0530

    Update prime2.py

--- prime2.py
+++ prime2.py
@@ -1,10 +1,10 @@
-a=int(raw_input())
-b=int(raw_input())
-for i in range(a,b):
-    if(a>1):
-        for i in range(2,b+1):
-        	if(a%i)==0:
-        	    break
-        	else:
-        		print(a)
-        		
+a = int(raw_input())
+b = int(raw_input())
+for num in range(a+1,b+1):
+   
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
