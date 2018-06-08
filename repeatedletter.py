e833072adeef84686a70980635459901fee38ae0
Pabitha <39622249+Pabitha@users.noreply.github.com>
2018-06-08 16:37:29 +0530

    Create repeatedletter.py

---
+++ repeatedletter.py
@@ -0,0 +1,14 @@
+a = 256
+def getMaxOccuringChar(s):
+    count =[0]*a
+    max = -1
+    c = ''
+    for i in s:
+        count[ord(i)]+=1;
+    for i in s:
+        if max < count[ord(i)]:
+            max = count[ord(i)]
+            c = i
+    return c
+s =raw_input()
+print((getMaxOccuringChar(s)))
