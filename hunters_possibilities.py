05df4ecda385e20f61a30939c0326c4c03f1bb1b
Pabitha <39622249+Pabitha@users.noreply.github.com>
2018-06-30 14:43:25 +0530

    Create hunters_possibilities.py

---
+++ hunters_possibilities.py
@@ -0,0 +1,15 @@
+def ab():
+	n=int(input())
+	l=[]
+	
+	for i in range(n):
+		l.append(int(input()))
+	for i in range(n):
+		for j in range(i+1,n):
+			for k in range(j+1,n):
+				if l[i]+l[j]==l[k]:
+					print(l[i],l[j],l[k])
+try:
+	ab()
+except:
+	print('invalid')
