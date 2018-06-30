580cb4300edb5200851899ef733916b29e34360a
Pabitha <39622249+Pabitha@users.noreply.github.com>
2018-06-30 15:34:02 +0530

    Create hunters_close.py

---
+++ hunters_close.py
@@ -0,0 +1,13 @@
+def closez():
+	n=int(input())
+	l=[]
+	for i in range(n):
+		l.append(int(input()))
+	for i in range(n):
+		for j in range(i+1,n):
+			if l[i]+l[j]==0 :
+				print(l[i],l[j])
+try:
+	closez()
+except:
+	print('invalid')
